759d35a4cf454434fd434c298a155e5f41defab7
slogan_codes <newson190@gmail.com>
2025-09-04 18:38:04 +0100

    Created the authetication views for register, login and logout

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 from . import views
-from .views import list_books, LibraryDetailView
+from .views import LibraryDetailView, RegisterView
+from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
     path('books/', views.books, name='books'),
-    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library-detail')
+    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library-detail'),
+    path('register/', RegisterView.as_view(), name='register'),
+    path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout')
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render
 from relationship_app.models import Book
 from .models import Library
 from django.views.generic.detail import DetailView
-
+from django.views.generic import CreateView
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
 # Create your views here.
 
 
@@ -17,3 +19,9 @@ class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
+
+
+class RegisterView(CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = 'relationship_app/register.html'
