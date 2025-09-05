8f91f9f97551aaa131931b81b11920fb9313d062
slogan_codes <newson190@gmail.com>
2025-09-05 01:02:46 +0100

    Put the role based functions in views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -4,9 +4,11 @@ from .models import Library
 from django.views.generic.detail import DetailView
 from django.views.generic import CreateView
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import user_passes_test
 from django.contrib.auth import login
 from django.contrib import messages
 from django.urls import reverse_lazy
+from .utils.role_checker import is_admin, is_member, is_librarian
 # Create your views here.
 
 
@@ -35,6 +37,20 @@ def register(request):
 def profile(request):
     return render(request, 'relationship_app/profile.html')
 
+
+
+
+@user_passes_test(is_admin)
+def adminview(request):
+    return render(request, 'relationship_app/admin_view.html')
+
+@user_passes_test(is_librarian)
+def librarianview(request):
+    return render(request, 'relationship_app/librarian_view.html')
+
+@user_passes_test(is_member)
+def memberview(request):
+    return render(request, 'relationship_app/member_view.html')
 # class register(CreateView):
 #     form_class = UserCreationForm
 #     success_url = reverse_lazy('login')
