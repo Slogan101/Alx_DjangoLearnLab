101c2633a8425a72361dfe3976f2e61c612c8be6
slogan_codes <newson190@gmail.com>
2025-09-08 11:09:33 +0100

    .

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -8,7 +8,7 @@ class Author(models.Model):
     name = models.CharField(max_length=100)
 
     def __str__(self):
-        return f'{self.name}'
+        return self.name
 
 class Book(models.Model):
     title = models.CharField(max_length=200)
