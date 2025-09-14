cb8c87fa2418298139c8818840ef7bb4d314d0b2
slogan_codes <newson190@gmail.com>
2025-09-14 09:14:51 +0100

    Added the customuseradmin

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from .models import Book, CustomUser
 
 # Register your models here.
@@ -6,7 +7,25 @@ class BookAdmin(admin.ModelAdmin):
     list_filter = ('title', 'author', 'publication_year')
     list_display = ('title', 'author', 'publication_year')
     search_fields = ('title', 'author')
-
     
 admin.site.register(Book, BookAdmin)
-admin.site.register(CustomUser)
+
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+
+    fieldsets = UserAdmin.fieldsets + (
+        ('Additional info', {
+            'fields': ('date_of_birth', 'profile_photo')
+        }),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ('Additional info', {
+            'fields': ('date_of_birth', 'profile_photo'),
+        }),
+    )
+
+    list_display = ['username', 'email', 'date_of_birth', 'is_staff']
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
