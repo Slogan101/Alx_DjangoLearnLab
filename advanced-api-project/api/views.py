c7801349225e7bd57bcfe83d6b7e52abb6833c1d
slogan_codes <newson190@gmail.com>
2025-09-27 11:52:23 +0100

    Added the api

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from .models import Book
 from rest_framework import generics
-from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly, AllowAny 
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly 
 from .serializers import BookSerializer
 
 # Create your views here.
