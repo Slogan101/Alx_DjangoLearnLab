336fa3495fd17c562ccc48a457c8d30271be9d55
slogan_codes <newson190@gmail.com>
2025-10-02 00:15:41 +0100

    Changed edit to update

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -25,6 +25,6 @@ urlpatterns = [
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-confirm-delete'),
     path('post/<int:post_id>/comments/new/', CommentCreateView.as_view(), name='add-comment'),
-    path('comments/<int:pk>/edit/', CommentUpdateView.as_view(), name='edit-comment'),
+    path('comments/<int:pk>/update/', CommentUpdateView.as_view(), name='edit-comment'),
     path('comments/<int:pk>/delete/', CommentDeleteView.as_view(), name='delete-comment'),
 ]
