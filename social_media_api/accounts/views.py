25966387835f7f9cebb7b4ceb7e6fd04131e49d7
slogan_codes <newson190@gmail.com>
2025-10-10 19:34:16 +0100

    Added the FOLLOW AND UNFOLLOW FEATURES

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -3,8 +3,10 @@ from rest_framework.response import Response
 from rest_framework.authtoken.models import Token
 from django.contrib.auth import authenticate
 from .serializers import RegisterSerializer, LoginSerializer, UserProfileSerializer
+from rest_framework.decorators import api_view, permission_classes
 from django.contrib.auth import get_user_model
 from rest_framework.permissions import IsAuthenticated
+from django.shortcuts import get_object_or_404
 
 User = get_user_model()
 
@@ -33,3 +35,22 @@ class ProfileView(generics.RetrieveUpdateAPIView):
 
     def get_object(self):
         return self.request.user
+
+# VIEW FOR FOLLOWING AND UNFOLLWOING AND ALSO SEEING THE POSTS OF ONLY THE USER YOU ARE FOLLLOWING!
+
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def follow_user(request, user_id):
+    target_user = get_object_or_404(User, id=user_id)
+    if target_user == request.user:
+        return Response({'error': 'You cannot follow yourself.'}, status=status.HTTP_400_BAD_REQUEST)
+    request.user.following.add(target_user)
+    return Response({'message': f'You are now following {target_user.username}.'})
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def unfollow_user(request, user_id):
+    target_user = get_object_or_404(User, id=user_id)
+    request.user.following.remove(target_user)
+    return Response({'message': f'You have unfollowed {target_user.username}.'})
